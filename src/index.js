2000764fd2cc25cb245d99509627d56342d8b3bb
Spaceflyy <115657060+Spaceflyy@users.noreply.github.com>
2023-07-11 13:11:29 +0100

    Add hi/lo temp/current weather/weather img Add basic css formatting

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-const search = document.getElementById("search");
-// const subBtn = document.getElementsByTagName("input[type=text]");
-const form = document.getElementById("form");
-const display = document.getElementById("tempDisplay");
+import "./style.css";
 
-const getData = async (location) => {
+const search = document.getElementById("search");
+
+const form = document.getElementById("form");
+const locDisplay = document.getElementById("locDisp");
+const temp = document.getElementById("temp");
+const weatherImg = document.getElementById("weatherImg");
+const currentWeather = document.getElementById("currentWeather");
+const HiLoTemp = document.getElementById("hiLoTemp");
+
+const getWeatherData = async (location) => {
 	try {
 		const response = await fetch(
-			` https://api.weatherapi.com/v1/current.json?key=f5ce7cba05f246508ad95118230507&q=${location}	`
+			` https://api.weatherapi.com/v1/forecast.json?key=f5ce7cba05f246508ad95118230507&q=${location}	`
 		);
+
 		const data = await response.json();
-		display.textContent = `${data.location.name} ${data.current.temp_c}\u00B0C`;
+
+		weatherImg.src = data.current.condition.icon;
+		locDisplay.textContent = `${data.location.name}`;
+		temp.innerText = `${data.current.temp_c}\u00B0C`;
+		currentWeather.innerText = `${data.current.condition.text} `;
+		HiLoTemp.innerText = `H:${data.forecast.forecastday[0].day.maxtemp_c}\u00B0C L:${data.forecast.forecastday[0].day.mintemp_c}\u00B0C`;
+		// , ${data.location.region}, ${data.location.country}
 	} catch (error) {
 		console.error(`Error: ${error}`);
 	}
@@ -17,12 +30,12 @@ const getData = async (location) => {
 
 const getlocation = async (lat, long) => {
 	const location = await fetch(
-		`https://api.opencagedata.com/geocode/v1/json?q=${lat}+${long}&key=af91cea604034c6db9a08743831a8590`
+		`https://api.opencagedata.com/geocode/v1/json?q=${String(lat)}+${String(
+			long
+		)}&key=af91cea604034c6db9a08743831a8590`
 	);
-
 	const data = await location.json();
-
-	getData(data.results[0].components.suburb);
+	getWeatherData(data.results[0].components.postcode);
 };
 
 const getCoords = () =>
@@ -41,5 +54,5 @@ getCoords()
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
 	const location = search.value;
-	getData(location);
+	getWeatherData(location);
 });

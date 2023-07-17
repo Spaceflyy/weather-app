4d942eddd135bb4b487e53736b15169462845419
Spaceflyy <115657060+Spaceflyy@users.noreply.github.com>
2023-07-17 17:40:23 +0100

    add air condition info

--- src/DOMFunctions.js
+++ src/DOMFunctions.js
@@ -4,14 +4,43 @@ const search = document.getElementById("search");
 const form = document.getElementById("form");
 const locDisplay = document.getElementById("locDisp");
 const temp = document.getElementById("temp");
-const currnetWeatherImg = document.getElementById("weatherImg");
+const currentWeatherImg = document.getElementById("weatherImg");
 const currentWeather = document.getElementById("currentWeather");
 const HiLoTemp = document.getElementById("hiLoTemp");
 const hourlyContainer = document.getElementById("hourlyContainer");
 const threedaycontainer = document.getElementById("threedayContainer");
 
-async function renderWeather(data) {
+const realFeel = document.getElementById("realFeel");
+const wind = document.getElementById("windSpeed");
+const rain = document.getElementById("rain");
+const uv = document.getElementById("uv");
+
+const renderHourlyWeather = (data) => {
 	const hourly = data.forecast.forecastday[0].hour;
+	hourly.forEach((element) => {
+		const container = document.createElement("div");
+		const time = document.createElement("p");
+		const hourTemp = document.createElement("p");
+		const weatherImg = new Image();
+		weatherImg.src = element.condition.icon;
+		if (element.time.slice(11, 13) === data.current.last_updated.slice(11, 13)) {
+			time.innerText = "Now";
+		} else {
+			time.innerText = element.time.slice(11);
+		}
+
+		console.log(`element.time: ${element.time.slice(11, 13)}`);
+		console.log(`data.current: ${data.current.last_updated.slice(11)}`);
+		hourTemp.innerText = `${element.temp_c}\u00B0C`;
+		container.append(time);
+		container.append(weatherImg);
+		container.append(hourTemp);
+		hourlyContainer.append(container);
+	});
+};
+
+const renderThreeDayForecast = (data) => {
+	console.log(data);
 	const dayForecasts = data.forecast.forecastday;
 	const weekDay = [
 		"Sunday",
@@ -22,29 +51,6 @@ async function renderWeather(data) {
 		"Friday",
 		"Saturday",
 	];
-	console.log(data);
-	currnetWeatherImg.src = data.current.condition.icon;
-	locDisplay.textContent = `${data.location.name}`;
-	temp.innerText = `${data.current.temp_c}\u00B0C`;
-	currentWeather.innerText = `${data.current.condition.text} `;
-	HiLoTemp.innerText = `H:${data.forecast.forecastday[0].day.maxtemp_c}\u00B0C L:${data.forecast.forecastday[0].day.mintemp_c}\u00B0C`;
-
-	hourly.forEach((element) => {
-		if (element.time.slice(11) > data.current.last_updated.slice(11)) {
-			const container = document.createElement("div");
-			const time = document.createElement("p");
-			const hourTemp = document.createElement("p");
-			const weatherImg = new Image();
-			weatherImg.src = element.condition.icon;
-			time.innerText = element.time.slice(11);
-			hourTemp.innerText = `${element.temp_c}\u00B0C`;
-			container.append(time);
-			container.append(weatherImg);
-			container.append(hourTemp);
-			hourlyContainer.append(container);
-		}
-	});
-
 	for (let i = 0; i < dayForecasts.length; i++) {
 		const day = document.createElement("p");
 		const hiTemp = document.createElement("p");
@@ -64,8 +70,22 @@ async function renderWeather(data) {
 		threedaycontainer.append(lowTemp);
 		threedaycontainer.append(hiTemp);
 	}
-}
+};
 
+async function renderWeather(data) {
+	currentWeatherImg.src = data.current.condition.icon;
+	locDisplay.textContent = `${data.location.name}`;
+	temp.innerText = `${data.current.temp_c}\u00B0C`;
+	currentWeather.innerText = `${data.current.condition.text} `;
+	realFeel.innerText = `${data.current.feelslike_c}\u00B0C`;
+	wind.innerText = `${data.current.wind_mph} MPH`;
+	rain.innerText = `${data.forecast.forecastday[0].day.daily_chance_of_rain} %`;
+	uv.innerText = `${data.current.uv}`;
+	HiLoTemp.innerText = `H:${data.forecast.forecastday[0].day.maxtemp_c}\u00B0C L:${data.forecast.forecastday[0].day.mintemp_c}\u00B0C`;
+
+	renderHourlyWeather(data);
+	renderThreeDayForecast(data);
+}
 const bindSearch = (handler) => {
 	form.addEventListener("submit", (e) => {
 		e.preventDefault();
